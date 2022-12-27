965e35e77d8046a87fb8cf206a645c0de022c645
Marcia Cripps <mcripps@optimedhp.com>
2022-12-27 08:16:10 -0500

    8kyu easy

---
+++ 12-27-22SuperDuperEasy.py
@@ -0,0 +1,13 @@
+# Make a function that returns the value multiplied by 50 and increased by 6. If the value entered is a string it should return "Error".
+
+def problem(a):
+    if type(a) == str: 
+        return 'Error'
+    else:
+        return (a*50) + 6
+
+
+#ternery below
+
+def problem(a):
+    return 'Error' if type(a) == str else (a*50) + 6
