a67cb8b8524d8b43f806d4b0240abb8c8919ee13
Marcia Cripps <mcripps@optimedhp.com>
2024-09-25 09:23:23 -0400

    Create 9-25-24VexingVanishingValues.py

---
+++ 9-25-24VexingVanishingValues.py
@@ -0,0 +1,23 @@
+# Confusion, perplexity, a mild headache. These are just a sample of the things you have experienced in the last few minutes while
+#  trying to figure out what is going on in your code.
+# The task is very simple: accept a list of values, and another value n, 
+# then return a new list with every value multiplied by n. For example, [1, 2, 3] and 4 should result in [4, 8, 12].
+# While writing the function, you even added some debugging lines to make sure that you didn't mess anything up, and everything looked good! 
+# But for some reason when you run the function it always seems to return an empty list,
+# even though you can clearly see, that the list should have the right values in it! Somehow,
+# the values are simply disappearing! Is this a bug in the programming language itself...?
+
+def mul_by_n(lst, n):
+    arr = []
+    for i in lst:
+        arr.append(i*n)
+    return arr
+
+# array of nums and a number
+#multiple everything  by the n 
+#return a new array 
+
+#dry version w a list comprehension 
+
+def mul_by_n(lst, n):
+    return [i*n for i in lst]
